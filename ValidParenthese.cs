98de086169d3d5af1b264893f9b7db66769babc2
BenPease21 <BAJA154@outlook.com>
2025-04-16 20:44:22 -0500

    Submit

---
+++ ValidParenthese.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections.Generic;
+
+
+// Idk why this one isn't working in Visual Studio,
+// it runs in the code, but its erroring out like crazy here
+
+
+class Solution
+{
+    public:
+    bool isValid(string s)
+    {
+        stack<char> stack;
+        unordered_map<char, char> bracketMap = {
+            {')', '('},
+            {']', '['},
+            {'}', '{'}
+        };
+
+        for (char c : s)
+        {
+            if (bracketMap.count(c))
+            {
+                if (stack.empty() || stack.top() != bracketMap[c])
+                {
+                    return false;
+                }
+                stack.pop();
+            }
+            else
+            {
+                stack.push(c);
+            }
+        }
+
+        return stack.empty();
+    }
+};
